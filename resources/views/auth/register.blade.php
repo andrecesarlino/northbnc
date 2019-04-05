05dee7c40048d9392fbe2bd7a1708456e67d916b
Andre <andreclino@gmail.com>
2019-04-05 15:19:19 -0300

    Create Client

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@
 
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-
+trytyrtyr
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
