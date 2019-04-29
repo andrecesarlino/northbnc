c87ee60a344449d8f3376d4ae47805089ea1074a
Andre <andreclino@gmail.com>
2019-04-29 17:14:20 -0300

    Criado visualizar, editar, deletar

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ClientsSeeder::class);
-        $this->call(ProductSeeder::class);
+       // $this->call(ClientsSeeder::class);
+       // $this->call(ProductSeeder::class);
+        $this->call(CategorySeed::class);
     }
 }
